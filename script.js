be20d467358e1b93509eba988b8edae57f09206f
Riley Rudd <Rileymakena@gmail.com>
2021-04-10 14:54:16 -1000

    reviewing functions

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
 }
 console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
 ///////////////////////////////////////
-// Functions Calling Other Functions*/
+// Functions Calling Other Functions
 
 function cutFruitPieces(fruit) {
   return fruit * 4;
@@ -92,3 +92,25 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+/////////////////////////////////////
+// Reviewing Functions */
+
+const calcAge = function(birthYear) {
+  return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName)  
+{
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired!`);
+    return -1;
+  }
+  //return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
