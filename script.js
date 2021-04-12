f287cb3ff69999a27781b942f8292ae85a370c4a
Riley Rudd <Rileymakena@gmail.com>
2021-04-11 14:38:02 -1000

    array methods

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
 /////////////////////////////////////////////////////////////////////////
-//////// Arrays */
+//////// Arrays
 
 const friend1= 'Michael';
 const friend2 = 'Steven';
@@ -183,3 +183,36 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
+//////////////////////////////////////////////////////////////////////////////////////
+/////// Array Methods */
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last Element
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Peter')) {
+  console.log('You have a friend called Peter');
+}
